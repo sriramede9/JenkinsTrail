d10be5fea8fba54fc6709717302135a0d856acc2
sriramede9 <only4programming03@gmail.com>
2019-12-10 22:46:03 -0500

    added to git mapped to jenkins

--- A.java
+++ A.java
@@ -1,5 +1,5 @@
 public class A{
 public static void main(String ar[]){
-System.out.println("Hello Sri How you doing?");
+System.out.println("Hello Sri How you doing? so added to Git and added trigger as well which is poll SCM");
 }
 }
